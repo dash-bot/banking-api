9bb2973fb9f33fe00ce53340a40f8e6cd19a42d4
AJ Po-Deziel <aj.podeziel@gmail.com>
2018-01-14 02:11:22 -0800

    Added DB balance query, DB connection

--- bank_api.py
+++ bank_api.py
@@ -5,9 +5,13 @@ a valid crypto-ticket from the auth server.
 """
 
 from flask import Flask, json, request
+from bank_db import BankDBConnection
+import psycopg2
 
 app = Flask(__name__)
 application = app
+
+bank_db = BankDBConnection()
 
 
 @app.route("/balance", methods=['GET'])
@@ -27,6 +31,20 @@ def balance():
     """
     uid = request.args["uid"]
     # TODO: verify uid
+
+    results = bank_db.execute(
+        """
+        SELECT a.account_id, u, a.acct_amount
+        FROM accounts a INNER JOIN account_type atype ON a.user_id = u.user_id WHERE a.user_id = %s AND atype.account_type LIKE %s;
+        """,
+        (uid, ))
+    amount_list = []
+
+    if results is not None:
+        for acct_amount in results:
+            amount_list.append(acct_amount)
+    else:
+        return None
 
     return json.jsonify({
         "balances": {
