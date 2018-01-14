229e11db7dcb4c0a34c87552130071a1bb53caea
Charlie Friend <charles.d.friend@gmail.com>
2018-01-13 16:30:48 -0800

    Add /balance route.

--- bank_api.py
+++ bank_api.py
@@ -4,14 +4,35 @@ This application enforces access control - every transaction must be sent with
 a valid crypto-ticket from the auth server.
 """
 
-from flask import Flask
+from flask import Flask, json, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def main():
-    return "Hello world!"
+@app.route("/balance", methods=['GET'])
+def balance():
+    """
+    Gets the balance for a given user.
+
+    Request parameters:
+        uid = USER ID
+
+    Response:
+        {
+            "balances" : {
+                (ACCOUNT_ID: ${balance}, )+
+            }
+        }
+    """
+    uid = request.args["uid"]
+    # TODO: verify uid
+
+    return json.jsonify({
+        "balances": {
+            1: 2000.0,
+            2: 12.0
+        }
+    })
 
 
 if __name__ == "__main__":
